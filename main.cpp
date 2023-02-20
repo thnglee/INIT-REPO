86949978cb3088efd6cd775c78660341b0f3f9e6
thnglee <fearthedeer1424@gmail.com>
2023-02-20 22:42:01 +0700

    changing main file one last time

--- main.cpp
+++ main.cpp
@@ -4,17 +4,9 @@
 using namespace std;
 
 int main() {
-	cout << "Hello World" << endl;
-	int n; 
-	cout << "Enter n: " << endl;
-	cin >> n;
-	
+	int n; cin >> n;
 	vector<int> list;
-	for (int i = 0; i < n; i++) {
-		list.push_back(i);
-	}
-	for (size_t i = 0; i < list.size(); i++){
-		cout << list[i] << " ";
-	}
+	for (int i = 0; i < n; i++) list.push_back(i);
+	for (size_t i = 0; i < list.size(); i++) cout << list[i] << " ";
 	return 0;
 }
