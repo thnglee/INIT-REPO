bf4aa0cc51829fe2d0b3c9c5843488bc8b5df7f7
thnglee <fearthedeer1424@gmail.com>
2023-02-20 21:30:23 +0700

    updating new main file

--- main.cpp
+++ main.cpp
@@ -1,19 +1,20 @@
 #include<iostream>
 #include<vector>
+
 using namespace std;
 
 int main() {
-	cout << "Enter a parameter: ";
-	int n; cin >> n;
+	cout << "Hello World" << endl;
+	int n; 
+	cout << "Enter n: " << endl;
+	cin >> n;
 	
 	vector<int> list;
 	for (int i = 0; i < n; i++) {
 		list.push_back(i);
 	}
-
-	for (size_t i = 0; i < list.size(); i++) {
+	for (size_t i = 0; i < list.size(); i++){
 		cout << list[i] << " ";
 	}
-
 	return 0;
 }
