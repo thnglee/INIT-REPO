7af3f66820c1e9731899ec673cf665d1dda7fa28
thnglee <fearthedeer1424@gmail.com>
2023-02-20 22:42:29 +0700

    main file changing

--- main.cpp
+++ main.cpp
@@ -4,9 +4,6 @@
 using namespace std;
 
 int main() {
-	int n; cin >> n;
-	vector<int> list;
-	for (int i = 0; i < n; i++) list.push_back(i);
-	for (size_t i = 0; i < list.size(); i++) cout << list[i] << " ";
+	cout << "hello world" << endl;
 	return 0;
 }
